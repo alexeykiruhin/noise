43d45147e6ed11a56b50345f59205e56888673a1
alexeykiruhin <q23sa3501080@gmail.com>
2020-01-26 00:07:09 +0300

    add some beta css styles

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import css from './Profile.module.css';
+import MyPosts from "./My posts/MyPosts";
 
 class Profile extends Component {
   render() {
     return(
       <div className={css.profile}>
-
+        <img src="https://nysurfsoccer.org/wp-content/uploads/2017/03/IMG.png" alt="img"/>
+        <MyPosts />
       </div>
     );
   }
