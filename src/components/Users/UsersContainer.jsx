422c2af2db0e1b374db557b931b459f09fd47b4a
alexeykiruhin <q23sa3501080@gmail.com>
2020-02-26 21:28:04 +0300

    delete UsersApi

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,4 +1,4 @@
-import UsersAPI from "./UsersAPI";
+import React, {Component} from "react";
 import {connect} from "react-redux";
 import {
     changeCurrentPageAC,
@@ -7,6 +7,42 @@ import {
     setUsersAC,
     unfollowAC
 } from "../../redux/users_reducer";
+import * as axios from "axios";
+import Pagination from "./Pagination/Pagination";
+import Users from "./Users";
+
+class UsersAPI extends Component {
+
+    getUsers = () => {
+        axios
+            .get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(res => {
+                this.props.setUsers(res.data.items);
+                if (this.props.totalUsersCount === 0){
+                    this.props.getUsersTotalCount(res.data.totalCount);
+                }
+            });
+    };
+
+    componentDidMount = () => {
+        this.getUsers();
+    };
+
+    render = () => {
+        return <div>
+            <div>
+                <Pagination totalUsersCount={this.props.totalUsersCount}
+                            pageSize={this.props.pageSize}
+                            currentPage={this.props.currentPage}
+                            getUsers={this.getUsers}
+                            changeCurrentPage={this.props.changeCurrentPage} />
+            </div>
+            <Users users={this.props.users}
+                   follow={this.props.follow}
+                   unfollow={this.props.unfollow} />
+        </div>
+    };
+}
 
 const mapStateToProps = (state) => {
     return {
