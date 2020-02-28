c76a490718d11b0f118195fa24fd3a4f6d8b1a75
alexeykiruhin <q23sa3501080@gmail.com>
2020-02-28 17:00:35 +0300

    refactoring class to functional component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,25 @@
-import React, {Component} from 'react';
+import React from 'react';
 import logo from './logo.png'
 import css from './Header.module.css';
 import {NavLink} from "react-router-dom";
 
-class Header extends Component {
+const Header = (props) => {
 
-    render() {
-        let login;
-        if (this.props.resultCode === 1){
-            login = <NavLink to={'/login'}>Login</NavLink>;
-        } else if(this.props.resultCode === 0) {
-            login = this.props.login
-        }
-        return (
-            <header className={css.header}>
-                <NavLink to={'/profile'}>
-                    <img src={logo} alt="logo"/>
-                </NavLink>
-                <div className={css.loginBlock}>{login}</div>
-            </header>
-        );
+    let login;
+    if (props.resultCode === 1) {
+        login = <NavLink to={'/login'}>Login</NavLink>;
+    } else if (props.resultCode === 0) {
+        login = props.login
     }
-}
+
+    return (
+        <header className={css.header}>
+            <NavLink to={'/profile'}>
+                <img src={logo} alt="logo"/>
+            </NavLink>
+            <div className={css.loginBlock}>{login}</div>
+        </header>
+    );
+};
 
 export default Header;
